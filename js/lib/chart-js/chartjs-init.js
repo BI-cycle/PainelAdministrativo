f9bd6265bced322a44eb72acb1c0ab6e7f35d7d6
Marcelo Barbosa <ma.barbosa@outlook.com>
2018-06-03 04:32:19 -0300

    Ocorrencias ao Vivo
    Inicio do desenvolvimento da tela de ocorrencias ao vivo.

--- js/lib/chart-js/chartjs-init.js
+++ js/lib/chart-js/chartjs-init.js
@@ -7,18 +7,18 @@
 	var myChart = new Chart( ctx, {
 		type: 'radar',
 		data: {
-			labels: [ "Iluminação", "Buraco na Via", "Acidente", "Assalto", "Mato na Via", "Sinalização" ],
+			labels: [ "Iluminação", "Buraco na Via", "Acidente", "Assalto", "Mato na Via", "Sinalização", "Obstáculo", "Limpeza", "Alagamento", "Segurança" ],
 			datasets: [
 				{
-					label: "Ocorrências Maio",
-					data: [ 65, 32, 66, 45, 56, 37 ],
+					label: "Mês Anterior",
+					data: [ 65, 32, 66, 45, 56, 37, 27, 41, 21, 33 ],
 					borderColor: "rgba(0, 123, 255, 0.6)",
 					borderWidth: "1",
                     backgroundColor: "rgba(239, 83, 80, 0.42)"
                             },
 				{
-					label: "Ocorrências Junho",
-					data: [ 28, 12, 40, 19, 63, 21 ],
+					label: "Mês Atual",
+					data: [ 28, 12, 40, 19, 63, 21, 14, 21, 11, 7],
 					borderColor: "rgba(0, 123, 255, 0.7",
 					borderWidth: "1",
 					backgroundColor: "rgba(0, 123, 255, 0.5)"
